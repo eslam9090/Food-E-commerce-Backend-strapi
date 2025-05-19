d895370a82757dd42184d6bf4f3386f4946cc986
eslam9090 <eslammyaakoup7090@gmail.com>
2025-05-19 16:43:08 +0300

    updates5

--- config/database.ts
+++ config/database.ts
@@ -63,14 +63,17 @@ module.exports = ({ env }) => ({
     client: "postgres",
     connection: {
       host: env("DATABASE_HOST"),
-      port: env.int("DATABASE_PORT", 5432),
+      port: env.int("DATABASE_PORT"),
       database: env("DATABASE_NAME"),
       user: env("DATABASE_USERNAME"),
       password: env("DATABASE_PASSWORD"),
-
       ssl: {
         rejectUnauthorized: false,
       },
+    },
+    pool: {
+      min: 0,
+      max: 3,
     },
   },
 });
