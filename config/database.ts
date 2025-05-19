fe09592c3d2b34e04f1e88aa69b0cfbcb9d27247
eslam9090 <eslammyaakoup7090@gmail.com>
2025-05-19 12:24:50 +0300

    new updates

--- config/database.ts
+++ config/database.ts
@@ -67,7 +67,9 @@ module.exports = ({ env }) => ({
       database: env("DATABASE_NAME"),
       user: env("DATABASE_USERNAME"),
       password: env("DATABASE_PASSWORD"),
-      ssl: env.bool("DATABASE_SSL", true),
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
   },
 });
