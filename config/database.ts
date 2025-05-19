45119a2cbe33487bb4dab3429995268fdd93d356
eslam9090 <eslammyaakoup7090@gmail.com>
2025-05-19 16:08:03 +0300

    new updates2

--- config/database.ts
+++ config/database.ts
@@ -63,7 +63,7 @@ module.exports = ({ env }) => ({
     client: "postgres",
     connection: {
       host: env("DATABASE_HOST"),
-      port: env.int("DATABASE_PORT"),
+      port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME"),
       user: env("DATABASE_USERNAME"),
       password: env("DATABASE_PASSWORD"),
